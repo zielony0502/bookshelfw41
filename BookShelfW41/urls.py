e52cb41c4c4e3d9f60bf08b2b03a9b43160da897
cooklee <sbbogus@gmail.com>
2025-06-14 11:32:53 +0200

    Templet view index

--- BookShelfW41/urls.py
+++ BookShelfW41/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
+from books import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name='base.html'), name="home"),
 ]
