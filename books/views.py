4cd97a0ac10da5612fa76ce15950f9173d13836e
zielony0502 <zielony0502@wp.pl>
2025-06-14 21:17:28 +0200

    Menu i lista autorów

--- books/views.py
+++ books/views.py
@@ -8,7 +8,8 @@ from books.models import Author
 
 class AddAuthorView(View):
     def get(self, request):
-        return render(request, 'books/add_author.html')
+        authors = Author.objects.all()
+        return render(request, 'books/add_author.html', {'authors': authors})
 
     def post(self, request):
         first_name = request.POST['first_name']
