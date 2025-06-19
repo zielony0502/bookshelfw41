949034bf1bb0c0dbffdcfe74fa753b20e7695d15
zielony0502 <zielony0502@wp.pl>
2025-06-19 23:46:42 +0200

    dodawanie rezyserow

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
+
+from movies.models import Director
 
 
 class AddMovieView(View):
@@ -10,3 +14,9 @@ class AddMovieView(View):
 class AddDirectorView(View):
     def get(self, request):
         return render(request, 'movies/add_director.html')
+
+    def post(self, request):
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        Director.objects.create(first_name=first_name, last_name=last_name)
+        return HttpResponseRedirect(reverse('dodaj_rezysera'))
