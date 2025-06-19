bbbde4b3643679fc1ecc34982646202c4a3e1275
zielony0502 <zielony0502@wp.pl>
2025-06-19 12:53:24 +0200

    strona dodawania filmów, menu - nieprawidowe

--- BookShelfW41/urls.py
+++ BookShelfW41/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('add_movie/', views.AddMovieView.as_view(), name='dodaj_film')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
+
+class AddMovieView(View):
+    def get(self, request):
+        return render(request, 'movies/add_movie.html')
