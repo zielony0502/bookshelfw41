eaade6abfc779e415c0996e51556bf5802e638d2
cooklee <sbbogus@gmail.com>
2025-06-29 14:04:27 +0200

    not valid test

--- books/tests.py
+++ books/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 
-from books.models import Author
+from books.models import Author, Publisher
 
 
 # Create your tests here.
@@ -24,3 +24,45 @@ def test_add_author_view_get(authors):
     assert response.context["authors"].count() == len(authors)
     for author in authors:
         assert author in response.context["authors"]
+
+@pytest.mark.django_db
+def test_add_author_view_post(authors):
+    c = Client()
+    url = reverse('add_author')
+    data = {
+        'first_name': 'John',
+        'last_name': 'Doe',
+    }
+    response = c.post(url, data)
+    assert response.status_code == 302
+    assert Author.objects.get(**data)
+
+
+@pytest.mark.django_db
+def test_add_publisher_post():
+    c = Client()
+    url = reverse('add_publisher')
+    data = {
+        'name':'ala_ma_kotas',
+        'year':1999
+    }
+    response = c.post(url, data)
+    assert response.status_code == 302
+    assert Publisher.objects.get(**data)
+
+
+@pytest.mark.django_db
+def test_add_publisher_post_not_valid():
+    c = Client()
+    url = reverse('add_publisher')
+    data = {
+        'name':'ala_ma_kota',
+        'year':1999
+    }
+
+    response = c.post(url, data)
+    form = response.context["form"]
+
+    assert response.status_code == 200
+    assert form.errors
+    assert not Publisher.objects.exists()
